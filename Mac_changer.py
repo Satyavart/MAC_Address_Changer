fa20f360e95f10ff8e7ec917a5607e8f7e3f1834
Satyavart <satyavarty591@gmail.com>
2020-02-27 21:26:09 +0530

    Prototype 3

--- Mac_changer.py
+++ Mac_changer.py
@@ -1,11 +1,12 @@
 #!usr/bin/env python
 
+import curses
 import time
 import subprocess
 import optparse
 import random
 import re
-
+import os
 
 def rand_mac():
     return "%02x:%02x:%02x:%02x:%02x:%02x" % (
@@ -28,17 +29,15 @@ def current_mac(interface):
 
 
 def change_add(interface, mac):
-    print("[+] Changing Mac address to " + mac)
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", mac])
-    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig", interface, "down"], stdout=open(os.devnull, 'wb'))
+    subprocess.call(["ifconfig", interface, "hw", "ether", mac], stdout=open(os.devnull, 'wb'))
+    subprocess.call(["ifconfig", interface, "up"], stdout=open(os.devnull, 'wb'))
     new_mac_address = current_mac(interface)
     if new_mac_address:
         if new_mac_address == mac:
-            print("[+] MAC address changed succesfully \n")
-        else:
-            print("[-] Error MAC address didn't change")
-    return
+            return 1
+    else:
+        return 0
 
 
 def check():
@@ -56,13 +55,34 @@ def check():
         elif options.random:
             new_mac = rand_mac()
             if not options.time_interval:
-                change_add(options.interface, new_mac)
-            elif options.time_interval:
-                while True:
-                    change_add(options.interface, new_mac)
-                    time1 = int(options.time_interval)
-                    time.sleep(time1)
-                    new_mac = rand_mac()
+                print("[+] Changing Mac address to " + new_mac)
+                cond0 = change_add(options.interface, new_mac)
+                if cond0 == 1:
+                    print("[+] MAC address changed succesfully")
+                else:
+                    print("[-] Error MAC address didn't change")
+            else:
+                stdscr = curses.initscr()
+                curses.noecho()
+                curses.cbreak()
+                try:
+                    while True:
+                        condition = change_add(options.interface, new_mac)
+                        stdscr.addstr(0, 0, "[+] Changing Mac address to {0}".format(new_mac))
+                        if condition == 1:
+                            stdscr.addstr(1, 0, "[+] MAC address changed succesfully")
+                        else:
+                            stdscr.addstr(1, 0, "[-] Error MAC address didn't change")
+                        stdscr.refresh()
+                        time1 = int(options.time_interval)
+                        time.sleep(time1)
+                        new_mac = rand_mac()
+                except KeyboardInterrupt:
+                    pass
+                finally:
+                    curses.echo()
+                    curses.nocbreak()
+                    curses.endwin()
         elif options.time_interval:
             par.error("[-] Cannot provide time unless random orientation is requested")
     elif options.new_mac:
@@ -71,7 +91,12 @@ def check():
         elif options.random:
             par.error("[-] Multiple MAC address given")
         else:
-            change_add(options.interface, options.new_mac)
+            print("[+] Changing Mac address to " + options.new_mac)
+            cond1 = change_add(options.interface, options.new_mac)
+            if cond1 == 1:
+                print("[+] MAC address changed succesfully")
+            else:
+                print("[-] Error MAC address didn't change")
 
 
 check()
